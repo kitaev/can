494569fee4fa69b45d999dfc2a691744de5729b7
Alexander Kitaev <kitaev@tmatesoft.com>
2013-05-09 14:02:11 +0200

    host: Simple CAN tranceiver implementation, baud rate 125Kb/s, 13/2/1 bit segments.

--- host_f4/src/main.c
+++ host_f4/src/main.c
@@ -7,7 +7,6 @@ void TIM2_IRQHandler(void) {
         return;
     }
     TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-    GPIO_ToggleBits(GPIOD, LED_PINS);
 }
 
 void EXTI0_IRQHandler(void) {
@@ -15,7 +14,27 @@ void EXTI0_IRQHandler(void) {
 		return;
 	}
     EXTI_ClearITPendingBit(EXTI_Line0);
-	GPIO_ToggleBits(GPIOD, LED_PINS);
+
+    CanTxMsg message;
+    message.StdId = 0x11;
+    message.RTR = CAN_RTR_DATA;
+    message.IDE = CAN_ID_STD;
+    message.DLC = 1;
+    message.Data[0] = 0x55;
+
+    uint8_t mailbox = CAN_Transmit(CAN2, &message);
+    if (mailbox == CAN_TxStatus_NoMailBox) {
+        GPIO_SetBits(GPIOD, GPIO_Pin_14);
+        return;
+    }
+    uint16_t timeout = 0x500;
+    while(CAN_TransmitStatus(CAN2, mailbox) != CANTXOK && timeout > 0) {
+    	timeout--;
+    }
+    if (timeout == 0) {
+    	GPIO_SetBits(GPIOD, GPIO_Pin_15);
+    }
+
 }
 
 void GPIO_Configure(uint32_t pins) {
@@ -34,6 +53,7 @@ void GPIO_Configure(uint32_t pins) {
 	GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 	// A0 Button
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 	GPIO_StructInit(&GPIO_InitStruct);
@@ -79,29 +99,83 @@ void TIM2_Configure(void) {
 
 	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 }
+void CAN2_RX0_IRQHandler(void) {
+	if (CAN_GetITStatus(CAN2, CAN_IT_FMP0) == RESET) {
+		return;
+	}
+    CAN_ClearITPendingBit(CAN2, CAN_IT_FMP0);
+
+    CanRxMsg message;
+    while(CAN_MessagePending(CAN2, 0)) {
+		CAN_Receive(CAN2, 0, &message);
+		if (message.DLC == 1 && message.Data[0] == 0xEF) {
+			GPIO_ToggleBits(GPIOD, LED_PINS);
+		}
+    }
+}
 
 void CAN_Configure(void) {
 	CAN_InitTypeDef CAN_InitStruct;
+	CAN_FilterInitTypeDef CAN_FilterInitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+	GPIO_InitTypeDef GPIO_InitStruct;
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	// can pins
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_CAN2);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_CAN2);
 
+	GPIO_StructInit(&GPIO_InitStruct);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	// can peripherial
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1 | RCC_APB1Periph_CAN2, ENABLE);
+
+	CAN_DeInit(CAN2);
 	CAN_StructInit(&CAN_InitStruct);
 	CAN_InitStruct.CAN_ABOM = DISABLE;
 	CAN_InitStruct.CAN_AWUM = DISABLE;
-	CAN_InitStruct.CAN_BS1 = CAN_BS1_8tq;
-	CAN_InitStruct.CAN_BS2 = CAN_BS2_7tq;
-	CAN_InitStruct.CAN_Mode = CAN_Mode_Normal;
 	CAN_InitStruct.CAN_NART = DISABLE;
-	CAN_InitStruct.CAN_Prescaler = 1;
 	CAN_InitStruct.CAN_RFLM = DISABLE;
-	CAN_InitStruct.CAN_SJW = CAN_SJW_1tq;
 	CAN_InitStruct.CAN_TTCM = DISABLE;
 	CAN_InitStruct.CAN_TXFP = DISABLE;
+	CAN_InitStruct.CAN_Prescaler = 21; // 125000 Baud rate assuimg Fpclk=42MHz
+	CAN_InitStruct.CAN_SJW = CAN_SJW_1tq;
+	CAN_InitStruct.CAN_BS1 = CAN_BS1_13tq;
+	CAN_InitStruct.CAN_BS2 = CAN_BS2_2tq;
+	CAN_InitStruct.CAN_Mode = CAN_Mode_Normal;
 	CAN_Init(CAN2, &CAN_InitStruct);
+
+	// can filter
+	CAN_FilterInitStruct.CAN_FilterNumber = 14;
+	CAN_FilterInitStruct.CAN_FilterMode = CAN_FilterMode_IdMask;
+	CAN_FilterInitStruct.CAN_FilterScale = CAN_FilterScale_16bit;
+	CAN_FilterInitStruct.CAN_FilterActivation = ENABLE;
+	CAN_FilterInitStruct.CAN_FilterFIFOAssignment = CAN_FIFO0;
+	CAN_FilterInitStruct.CAN_FilterIdHigh = 0;
+	CAN_FilterInitStruct.CAN_FilterIdLow = 0;
+	CAN_FilterInitStruct.CAN_FilterMaskIdHigh = 0;
+	CAN_FilterInitStruct.CAN_FilterMaskIdLow = 0;
+	CAN_FilterInit(&CAN_FilterInitStruct);
+
+	// can interrupt
+	CAN_ITConfig(CAN2, CAN_IT_FMP0, ENABLE);
+
+	NVIC_InitStruct.NVIC_IRQChannel = CAN2_RX0_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStruct);
 }
 
 void main(void){
 	GPIO_Configure(LED_PINS);
+	CAN_Configure();
 
 //	TIM2_Configure();
 //	TIM_Cmd(TIM2, ENABLE);
